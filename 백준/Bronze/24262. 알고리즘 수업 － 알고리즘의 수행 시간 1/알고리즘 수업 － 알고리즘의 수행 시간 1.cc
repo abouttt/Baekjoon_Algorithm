e9a7fb9517d1dfb32825d7b71a1b06a721a5ff44
Daehan Jang <abouttt57@gmail.com>
2025-01-25 18:59:03 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+#define FAST_IO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+int main()
+{
+	FAST_IO;
+
+	int n;
+	cin >> n;
+
+	cout << 1 << '\n' << 0;
+}
